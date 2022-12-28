2d4b3747ab0bbe183b815b6115ae14ac8ab69de2
Rodrigo Soca <socarodrigo2@gmail.com>
2022-12-28 19:40:32 -0300

    tarea-1

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BOLIVIANO
         </p>
         <a
           className="App-link"
@@ -19,6 +20,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
